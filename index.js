5e256629a4b0ada3c6555673149e274e505e5893
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-06-08 14:10:20 -0400

    commit tree 7f642c0d1f69a5fc7db8176151ba762f2870a6d6

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet = 400){
+  let freeSample;
+  if (feet <= 400){
+    return 'This one is on me!'
+  }
+  if (feet <= 2001){
+    return "I will gladly take your thirty bucks."
+  }
+ else if (feet > 2500){
+    return "No can do."
+  }
+  
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(travel){ 
+ return (travel == "NYC")? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  let charm;
+  switch(tips){
+    case "generous":
+      charm = "Thank you so much.";
+      break;
+      case "not as generous":
+        charm = 'Thank you.';
+        break;
+        default:
+          charm = "Bye."
+  }
+  return charm;
+ 
 }
